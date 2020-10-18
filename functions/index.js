732b545cb98372b571a61036b1270e1d59336cce
leandrocodes <leandrovianacodes@gmail.com>
2020-10-17 23:48:16 -0400

    server index

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,12 @@
-const functions = require('firebase-functions');
+const express = require('express')
+const functions = require('firebase-functions')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const app = express()
+
+app.use(cors())
+app.use(require('./routes'))
+app.use(bodyParser.json())
+
+exports.api = functions.https.onRequest(app)
