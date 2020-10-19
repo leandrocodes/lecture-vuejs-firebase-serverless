3e3262efb1a85f9edb7601893e45ebe1598c46a6
leandrocodes <leandrovianacodes@gmail.com>
2020-10-18 21:50:12 -0400

    retrieving data from backend

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,25 +3,12 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/prettier'],
+  extends: ['plugin:vue/essential', '@vue/standard'],
   parserOptions: {
     parser: 'babel-eslint'
   },
   rules: {
-    'prettier/prettier': [
-      'warn',
-      {
-        singleQuote: true,
-        semi: false,
-        trailingComma: 'none'
-      }
-    ],
-    'newline-per-chained-call': [
-      'error',
-      {
-        ignoreChainWithDepth: 5
-      }
-    ],
+    'space-before-function-paren': ['error', 'never'],
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,64 @@
 <template>
-  <router-view />
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+    <ul>
+      <li v-for="product in products" :key="product.id">
+        <div class="product__data">
+          {{ product.data.name }}
+        </div>
+        <div class="product__actions">
+          <span>Ver mais</span>
+          <span>Alterar</span>
+          <span>Excluir</span>
+        </div>
+      </li>
+    </ul>
+  </div>
 </template>
 
-<style lang="scss"></style>
+<script>
+export default {
+  name: 'App',
+  data: () => ({
+    products: []
+  }),
+  async created() {
+    const { data } = await this.$axios.get('products')
+    this.products = data
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: sans-serif;
+}
+
+ul {
+  list-style: none;
+}
+
+ul li {
+  box-shadow: 0 3px 3px rgba(0, 0, 0, 0.2);
+  padding: 2rem 1rem;
+  margin: 2rem 0;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.product__actions span {
+  margin: 0 0.25rem;
+}
+
+.product__actions span:nth-child(1) {
+  color: blue;
+}
+.product__actions span:nth-child(2) {
+  color: orange;
+}
+.product__actions span:nth-child(3) {
+  color: red;
+}
+</style>
